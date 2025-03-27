778e20a27a046150dcece1aa5d8a7831bfcbcf5d
Your Name <you@example.com>
2025-03-27 22:15:43 +0700

    first commit

--- www/main.mjs
+++ www/main.mjs
@@ -1,9 +1,14 @@
-import { CameraUtils, ShareAPI,Notification } from "./utils.mjs";
+import { CameraUtils, ShareAPI, Notification } from "./utils.mjs";
+
+const share = new ShareAPI();
+const cameraUtils = new CameraUtils();
+const notification = new Notification();
 
 // Chờ DOM được tải xong
 document.addEventListener("DOMContentLoaded", function () {
+  const calculateAgeBtn = document.getElementById("calculateAgeBtn");
+  calculateAgeBtn.onclick = calculateAge;
   //Phần camera
-  const cameraUtils = new CameraUtils();
   const takePictureBtn = document.getElementById("takePictureBtn");
   const photoEl = document.getElementById("photo");
   cameraUtils.apply(
@@ -15,20 +20,57 @@ document.addEventListener("DOMContentLoaded", function () {
       photoEl.style.display = "block";
     },
     (errors) => {
-      console.log(image);
+      console.log(errors);
     }
   );
   //Phần thông báo
-  const notification = new Notification();
   const notificationBtn = document.getElementById("notificationBtn");
   notificationBtn.onclick = async () => {
     console.log("hi");
     await notification.sendNotification("Xin chào Bích Thùy", "QTh mập", "");
   };
-  // //Phần share
-  const share = new ShareAPI();
+  //Phần share
   const shareBtn = document.getElementById("shareBtn");
   shareBtn.onclick = async () => {
-    await share.shareContent("Xin chào Bích Thùy", "Qth mập", "https://www.facebook.com/ronial.04?locale=vi_VN");
+    await share.shareContent(
+      "Xin chào Bích Thùy",
+      "Qth mập",
+      "https://www.facebook.com/ronial.04?locale=vi_VN"
+    );
   };
 });
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+async function calculateAge() {
+  const birthYear = document.getElementById("birthYear").value;
+  // const currentYear = new Date().getFullYear();
+
+  // if (birthYear && birthYear >= 1900 && birthYear <= currentYear) {
+  //   const age = currentYear - birthYear;
+  //   const text = `Bạn ${age} tuổi.`;
+  //   document.getElementById("result").innerText = text;
+  //   await notification.sendNotification("Xin chào Bích Thùy", text, "");
+  // } else {
+  //   document.getElementById("result").innerText =
+  //     "Vui lòng nhập năm sinh hợp lệ!";
+  //   await notification.sendNotification(
+  //     "Xin chào Bích Thùy",
+  //     "Vui lòng nhập năm sinh hợp lệ!",
+  //     ""
+  //   );
+  // }
+  const f = celsiusToFahrenheit(birthYear);
+  document.getElementById("result").innerText = f;
+  await notification.sendNotification(
+    "Xin chào Bích Thùy",
+    `Độ F hiện tại ${f}F`,
+    ""
+  );
+  await share.shareContent(
+    "Xin chào Bích Thùy",
+    `Độ F hiện tại ${f}F`,
+    "https://www.facebook.com/ronial.04?locale=vi_VN"
+  );
+}
